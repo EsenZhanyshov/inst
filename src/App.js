3dc776bdb2248232c7cb59849744ad6514eb9a41
Esen Zhanyshov <e.zhanyshov@gmail.com>
2024-02-12 16:02:45 +0600

    images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import MainRoutes from "./routes/MainRoutes";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello its me</h1>
+      <MainRoutes />
     </div>
   );
 };
